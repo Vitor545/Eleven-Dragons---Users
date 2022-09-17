9bfe5ffd1720938afbb7ddf9f2a0a64023fee62b
vitor <tutornews2@gmail.com>
2022-09-17 17:50:08 -0300

    feat: search bar

--- components/Users/index.tsx
+++ components/Users/index.tsx
@@ -1,77 +1,33 @@
 import style from './Users.module.scss'
 import Card from '../Card'
+import IUser from '../../types/User.interface'
+import { useState } from 'react'
 
-export default function TextInfinite() {
+export type UserProps = {
+  users: IUser[]
+}
+
+export default function Users({users}: UserProps) {
   const status_button: string = 'status_button_active'
+
   return (
     <section className={style.users}>
-      <div className='container'>
+      <div className="container">
         <div className={style.users_status}>
           <span>Filtrar por:</span>
           <a className={`${style[status_button]}`}>ATIVO</a>
           <a>INATIVO</a>
         </div>
         <div className={style.card_container}>
-          <Card
-            name={'Vitor Souza da Silva'}
-            gender={'male'}
-            email={'tutornews2@gmail.com'}
-            status={'inactive'}
-          />
-          <Card
-            name={'Vitor Souza da Silva'}
-            gender={'male'}
-            email={'tutornews2@gmail.com'}
-            status={'active'}
-          />
-          <Card
-            name={'Vitor Souza da Silva'}
-            gender={'male'}
-            email={'tutornews2@gmail.com'}
-            status={'active'}
-          />
-          <Card
-            name={'Vitor Souza da Silva'}
-            gender={'male'}
-            email={'tutornews2@gmail.com'}
-            status={'active'}
-          />
-          <Card
-            name={'Vitor Souza da Silva'}
-            gender={'male'}
-            email={'tutornews2@gmail.com'}
-            status={'active'}
-          />
-          <Card
-            name={'Vitor Souza da Silva'}
-            gender={'male'}
-            email={'tutornews2@gmail.com'}
-            status={'inactive'}
-          />
-          <Card
-            name={'Vitor Souza da Silva'}
-            gender={'male'}
-            email={'tutornews2@gmail.com'}
-            status={'active'}
-          />
-          <Card
-            name={'Vitor Souza da Silva'}
-            gender={'male'}
-            email={'tutornews2@gmail.com'}
-            status={'active'}
-          />
-          <Card
-            name={'Vitor Souza da Silva'}
-            gender={'male'}
-            email={'tutornews2@gmail.com'}
-            status={'active'}
-          />
-          <Card
-            name={'Vitor Souza da Silva'}
-            gender={'male'}
-            email={'tutornews2@gmail.com'}
-            status={'active'}
-          />
+          {users.map((user: IUser) => (
+            <Card
+              name={user.name}
+              gender={user.gender}
+              email={user.email}
+              status={user.status}
+              key={user.id}
+            />
+          ))}
         </div>
       </div>
     </section>
---
+++ store/Users.store.ts
@@ -0,0 +1,33 @@
+import {AppDispatch} from './index';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import axios from 'axios';
+import IUser from '../types/User.interface';
+
+const users = createSlice({
+	name: 'users',
+	initialState: {
+		users: [] as IUser[],
+	},
+	reducers: {
+		getUsers(state, action: PayloadAction<IUser[]>) {
+			state.users = action.payload;
+		},
+		addUsers(state, action: PayloadAction<IUser>) {
+			state.users = [...state.users, action.payload];
+		}
+	},
+});
+
+export const {
+	getUsers,
+	addUsers
+} = users.actions;
+
+export default users.reducer;
+
+export function asyncGetUsers(): any {
+	return async function (dispatch: AppDispatch) {
+		const response = await axios.get('https://gorest.co.in/public/v2/users');
+		dispatch(getUsers(response.data as IUser[]));
+	};
+}
