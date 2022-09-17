bc7324c493b6c12ce7e1deffc19624d65f067e86
vitor <tutornews2@gmail.com>
2022-09-17 18:04:12 -0300

    feat: button status style

--- components/Users/index.tsx
+++ components/Users/index.tsx
@@ -8,15 +8,35 @@ export type UserProps = {
 }
 
 export default function Users({users}: UserProps) {
+  const [isActive, setIsActive] = useState(false)
+  const [isInactive, setIsInactive] = useState(false)
   const status_button: string = 'status_button_active'
+
+  const onClick = (type: string) => {
+    if(type === 'active') {
+      if(isActive) {
+        setIsActive(false)
+      } else {
+        setIsActive(true)
+        setIsInactive(false)
+      }
+    } else {
+      if(isInactive) {
+        setIsInactive(false)
+      } else {
+        setIsInactive(true)
+        setIsActive(false)
+      }
+    }
+  }
 
   return (
     <section className={style.users}>
       <div className="container">
         <div className={style.users_status}>
           <span>Filtrar por:</span>
-          <a className={`${style[status_button]}`}>ATIVO</a>
-          <a>INATIVO</a>
+          <a className={isActive && `${style[status_button]}`} onClick={() => onClick('active')}>ATIVO</a>
+          <a className={isInactive && `${style[status_button]}`} onClick={() => onClick('inactive')}>INATIVO</a>
         </div>
         <div className={style.card_container}>
           {users.map((user: IUser) => (
