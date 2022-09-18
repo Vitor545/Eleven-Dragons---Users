6f2e001b9b145566e9b35943f208d9736154e41b
vitor <tutornews2@gmail.com>
2022-09-17 22:43:02 -0300

    feat: create page user create

---
+++ pages/create/index.tsx
@@ -0,0 +1,11 @@
+import { Provider } from 'react-redux';
+import CreateUserPage from '../../components/CreateUserPage';
+import store from "../../store";
+
+export default function CreateUser() {
+  return (
+    <Provider store={store}>
+      <CreateUserPage />
+    </Provider>
+  )
+}
