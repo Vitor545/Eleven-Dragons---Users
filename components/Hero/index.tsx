44b7c95a7522e74d67d762f2b9922ba6138447b0
vitor <tutornews2@gmail.com>
2022-09-18 19:23:05 -0300

    feat: responsible

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,10 +1,27 @@
 import style from './Hero.module.scss'
+import Typewriter from 'typewriter-effect'
 
 export default function Hero() {
   return (
     <section className={style.hero}>
       <div className={`container ${style.hero__container}`}>
-        <h1>Os <span>Melhores</span> Usuários</h1>
+      <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(1000)
+                .typeString('<h1>Os <span>Melhores</span> Usuários</h1>')
+                .pauseFor(15000)
+                .deleteChars(20)
+                .typeString('<h1>As <span>Melhores</span> Pessoas</h1>')
+                .pauseFor(15000)
+                .deleteChars(19)
+                .typeString('<h1>Os <span>Melhores</span> Amigos</h1>')
+                .pauseFor(15000)
+                .deleteChars(18)
+                .start()
+            }}
+            options={{ loop: true }}
+          />
       </div>
     </section>
   )
